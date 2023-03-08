ed4d5a9e37a550809ddcc4712e28c3f8b3d0b828
Saikiran8844 <nannapanenisaikiran8844@gmail.com>
2023-03-08 13:58:12 +0530

    Database Setup

---
+++ database.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+// connect to MongoDB Atlas
+mongoose.connect('mongodb+srv://root:root@database.ptdziec.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// define schema for subscribers collection
+const subscriberSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// create subscribers collection model
+const Subscriber = mongoose.model('Subscriber', subscriberSchema);
+
+module.exports = Subscriber;
