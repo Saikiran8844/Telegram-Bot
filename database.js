07b3fa1ff4501d55db8c9c6a64869b820e195a0e
Saikiran8844 <nannapanenisaikiran8844@gmail.com>
2023-03-09 15:51:36 +0530

    Packages

--- database.js
+++ database.js
@@ -32,10 +32,10 @@ module.exports = Subscriber;
 
 
 // Retrieve all documents from subscribers collection
-/*Subscriber.find({})
+Subscriber.find({})
   .then((subscribers) => {
     console.log('Subscribers:', subscribers);
   })
   .catch((error) => {
     console.error('Error retrieving subscribers:', error);
-  });*/
+  });
